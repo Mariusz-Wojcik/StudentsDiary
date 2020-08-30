369ea08bd626088c5332a78b527fe3402bfd6910
Mariusz Wójcik <36dragan@gmail.com>
2020-08-30 13:18:30 +0200

    Initial commit

---
+++ StudentsDiary/AddEdditStudent.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace StudentsDiary
+{
+    public partial class AddEdditStudent : Form
+    {
+        private int _studentId;
+        private Student _student;
+
+        private FileHelper<List<Student>> _fileHelper = new FileHelper<List<Student>>(Program.FilePath);
+        public AddEdditStudent(int id = 0)
+        {
+            InitializeComponent();
+
+            _studentId = id;
+            GetStudentData();
+            tbFirstName.Select();
+        }
+        private void GetStudentData()
+        {
+            if (_studentId != 0)
+            {
+                Text = "Edytowanie danych ucznia";
+                var students = _fileHelper.DeserializeFromFile();
+                _student = students.FirstOrDefault(x => x.Id == _studentId);
+
+                if (_student == null)
+
+                    throw new Exception("Brak użytkownika o podanym Id");
+
+                FillTextBoxes();
+            }
+        }
+        private void FillTextBoxes()
+        {
+            tbId.Text = _student.Id.ToString();
+            tbFirstName.Text = _student.FirstName;
+            tbLastName.Text = _student.LastName;
+            rtbComments.Text = _student.Comments;
+            tbMath.Text = _student.Math;
+            tbTechnology.Text = _student.Technology;
+            tbPhysics.Text = _student.Physics;
+            tbPolishLang.Text = _student.PolishLang;
+            tbForeignLang.Text = _student.ForeignLang;
+        }
+        private async void btnConfirm_Click(object sender, EventArgs e)
+        {
+            var students = _fileHelper.DeserializeFromFile();
+
+            if (_studentId != 0)
+                students.RemoveAll(x => x.Id == _studentId);
+            else
+                AssignIdToNewStudent(students);
+
+            AddNewUserToList(students);
+
+            _fileHelper.SerializeToFile(students);
+
+            await LongProcesAsync();
+
+            Close();
+        }
+        private async Task LongProcesAsync()
+        {
+           await Task.Run(() =>
+            {
+                Thread.Sleep(3000);
+            });
+            
+        }
+
+        private void AddNewUserToList(List<Student> students)
+        {
+            var student = new Student
+            {
+                Id = _studentId,
+                FirstName = tbFirstName.Text,
+                LastName = tbLastName.Text,
+                Comments = rtbComments.Text,
+                Math = tbMath.Text,
+                Technology = tbTechnology.Text,
+                Physics = tbPhysics.Text,
+                PolishLang = tbPolishLang.Text,
+                ForeignLang = tbForeignLang.Text
+            };
+
+            students.Add(student);
+        }
+
+        private void AssignIdToNewStudent(List<Student> students)
+        {
+            var studentWithHigherId = students.OrderByDescending(x => x.Id).FirstOrDefault();
+
+            _studentId = studentWithHigherId == null ?
+                1 : studentWithHigherId.Id + 1;
+        }
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ StudentsDiary/Main.cs
@@ -0,0 +1,123 @@
+﻿using StudentsDiary.Properties;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+
+namespace StudentsDiary
+{
+    public partial class Main : Form
+    {
+        private FileHelper<List<Student>> _fileHelper = new FileHelper<List<Student>>(Program.FilePath);
+
+        public bool IsMaximize
+        {
+            get
+            {
+                return Settings.Default.IsMaximize;
+            }
+            set 
+            {
+                Settings.Default.IsMaximize = value;
+            }
+        }
+
+        public Main()
+        {
+            InitializeComponent();
+            RefreshDiary();
+            SetColumnHeader();
+            if (IsMaximize)
+                WindowState = FormWindowState.Maximized;
+        }
+
+        private void RefreshDiary()
+        {
+            var students = _fileHelper.DeserializeFromFile();
+            dgvDiary.DataSource = students;
+        }
+
+        private void SetColumnHeader()
+        {
+            dgvDiary.Columns[0].HeaderText = "Numer";
+            dgvDiary.Columns[1].HeaderText = "Imię";
+            dgvDiary.Columns[2].HeaderText = "Nazwisko";
+            dgvDiary.Columns[3].HeaderText = "Uwagi";
+            dgvDiary.Columns[4].HeaderText = "Matematyka";
+            dgvDiary.Columns[5].HeaderText = "Technologia";
+            dgvDiary.Columns[6].HeaderText = "Fizyka";
+            dgvDiary.Columns[7].HeaderText = "Język polski";
+            dgvDiary.Columns[8].HeaderText = "Język obcy";
+        }
+
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            var addEditStuden = new AddEdditStudent();
+            addEditStuden.FormClosing += AddEditStuden_FormClosing;
+            addEditStuden.ShowDialog();
+        }
+
+        private void AddEditStuden_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            RefreshDiary();
+        }
+
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            if (dgvDiary.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Zaznacz ucznia którego dane chcesz edytować");
+                return;
+            }
+
+            var addEditStuden = new AddEdditStudent(Convert.ToInt32(dgvDiary.SelectedRows[0].Cells[0].Value));
+            addEditStuden.FormClosing += AddEditStuden_FormClosing;
+            addEditStuden.ShowDialog();
+
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            if (dgvDiary.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Zaznacz ucznia którego dane chcesz usunąć");
+                return;
+            }
+
+            var selectedStudent = dgvDiary.SelectedRows[0];
+            var confirmDelete =
+                MessageBox.Show($"Czy na pewno chcesz usunąć ucznia " +
+                $"{(selectedStudent.Cells[1].Value.ToString() + " " + selectedStudent.Cells[2].Value.ToString()).Trim()}",
+                "Usuwanie ucznia",
+                MessageBoxButtons.OKCancel);
+
+            if (confirmDelete == DialogResult.OK)
+            {
+                DeleteStudent(Convert.ToInt32(selectedStudent.Cells[0].Value));
+                RefreshDiary();
+            }
+        }
+
+        private void DeleteStudent(int id)
+        {
+            var students = _fileHelper.DeserializeFromFile();
+            students.RemoveAll(x => x.Id == id);
+            _fileHelper.SerializeToFile(students);
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            RefreshDiary();
+        }
+
+        private void Main_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (WindowState == FormWindowState.Maximized)
+                IsMaximize = true;
+            else
+                IsMaximize = false;
+            Settings.Default.Save();
+        }
+    }
+}
